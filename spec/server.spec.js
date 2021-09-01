c578af07367a4a13020a01a6ba2191e6d823e830
jaosso <jan.ossowski@gmx.de>
2021-09-01 12:26:05 +0200

    Initial stuff and Server spec and implementation

---
+++ spec/server.spec.js
@@ -0,0 +1,20 @@
+var request = require("supertest");
+require = require("really-need");
+
+describe("Starting express server", function () {
+  var server;
+  beforeEach(function () {
+    server = require('../app/server', { bustCache: true });
+  });
+  afterEach(function () {
+    return server.close();
+  });
+
+  test("Server responses to /, ", function (done) {
+    request(server).get("/").expect(200, done);
+  });
+
+  test("404 everything else, ", function (done) {
+    request(server).get("/foo/bar").expect(404, done);
+  });
+});
