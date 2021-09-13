e34320a7ef4bebdef98f2511c45f866c2fa0cb42
jaosso <jan.ossowski@gmx.de>
2021-09-13 13:55:46 +0200

    finished board and started board controller

--- app/controllers/board_controller.js
+++ app/controllers/board_controller.js
@@ -1,9 +1,26 @@
-class BoardFactory {
-    constructor() {}
+var Board = require('../models/board').Board;
 
-    createBoard() {
-        return new Board();
+class BoardFactory {
+  constructor() {}
+
+  create(shape, prime_color, second_color) {
+    return new Board(shape, this, this.calcPath(shape), prime_color, second_color);
+  }
+
+  calcPath(shape) {
+    var path = { length: null, path: [] };
+
+    // find start tile
+    var width = shape[0].length;
+    var height = shape.length;
+
+    for (var x = 0; x < width; x++) {
+      for (var y = 0; y < height; y++) {
+        console.log(x + ' ' + y);
+      }
     }
+    return path;
+  }
 }
 
 module.exports.BoardFactory = BoardFactory;
--- spec/controllers/board_controller.spec.js
+++ spec/controllers/board_controller.spec.js
@@ -1,19 +1,51 @@
-var BoardFactory =
-  require('../../app/controllers/board_controller').BoardFactory;
+var boardSettings = require("../spec_variables").boardSettings;
 
-describe('Board class ', function () {
-  test('when nothing, succeeds', function () {
+var BoardFactory =
+  require("../../app/controllers/board_controller").BoardFactory;
+
+describe("Board class ", function () {
+  test("when nothing, succeeds", function () {
     expect(true).toBeTruthy();
   });
 });
 
-describe('BoardFactory creates Board properly ', function () {
+describe("BoardFactory creates Board properly ", function () {
   var boardFactory;
   beforeAll(function () {
     boardFactory = new BoardFactory();
   });
 
-  test(' when all params are valid, succeeds', function () {
-    var validBoard = boardFactory.create();
+  test(" when all params are valid, succeeds", function () {
+    var validBoard = boardFactory.create(
+      boardSettings.shape,
+      boardSettings.prime_color,
+      boardSettings.second_color
+    );
+
+    expect(validBoard.getShape()).toEqual(boardSettings.shape);
+    expect(validBoard.getPath()).toEqual([
+      { x: 0, y: 0 },
+      { x: 1, y: 0 },
+      { x: 2, y: 0 },
+      { x: 3, y: 0 },
+      { x: 4, y: 0 },
+      { x: 5, y: 0 },
+      { x: 5, y: 1 },
+      { x: 5, y: 2 },
+      { x: 5, y: 3 },
+      { x: 5, y: 4 },
+      { x: 5, y: 5 },
+      { x: 4, y: 5 },
+      { x: 3, y: 5 },
+      { x: 2, y: 5 },
+      { x: 1, y: 5 },
+      { x: 0, y: 5 },
+      { x: 0, y: 4 },
+      { x: 0, y: 3 },
+      { x: 0, y: 2 },
+      { x: 0, y: 1 },
+    ]);
+    expect(validBoard.getPrimeColor()).toEqual(boardSettings.prime_color);
+    expect(validBoard.getSecondColor()).toEqual(boardSettings.second_color);
   });
 });
